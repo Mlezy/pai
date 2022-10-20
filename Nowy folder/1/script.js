7f8960efe5dac7875f073bc880bd3959fbefdf38
Bartek Serafin <bartek.hugo1@gmail.com>
2022-10-20 14:30:57 +0200

    komentarz

---
+++ Nowy folder/1/script.js
@@ -0,0 +1,5 @@
+ if (confirm("czy lubisz pizze")) {
+   alert("Wybrano opcję: jaką?");
+   } else {
+   alert("Wybrano opcję: szkoda");
+ } 
---
+++ Nowy folder/2/java.js
@@ -0,0 +1,12 @@
+ var wysokosc = parseInt(prompt("podaj wysokość prostokąta"))
+ var szerokosc = parseInt(prompt("podaj szerokość prostokąta"))
+
+document.write("wysokość="+wysokosc+"<br/>");
+document.write("szerokość="+szerokosc+"<br/>");
+
+for (a = 1; a <= wysokosc; ++a){
+  for (count = 1; count <= szerokosc; ++count){
+        document.write("*"); 
+    }
+    document.write("<br/>"); 
+  }
